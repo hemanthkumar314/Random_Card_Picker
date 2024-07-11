969a92a02f836f7e120ecbc7623587c394550675
hemanthkumar314 <bhemanthkumar314@gmail.com>
2024-07-11 15:15:33 +0530

    Still Updating

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {Pool} = require('pg');
 
 const app = express();
 const port = 3000;
+var ids=[]
 
 const sessionSecret ="your_secret_key";
 
@@ -29,29 +30,31 @@ app.use(passport.session());
 const db = new pg.Client({
     user: "postgres",
     host: "localhost",
-    database: "dramatics",
+    database: "random_card",
     password: "April@17#21",
     port: 1721
 });
 
-
 db.connect();
 
-// Routes
 app.get("/", (req, res) => {
     res.render("login.ejs");
 });
-
 
 app.post("/register", async (req, res) => {
     const user = req.body.username;
     const email = req.body.email;
     const pass = req.body.password;
     const account = req.body.account;
-    const storyline = "story line";
 
     try {
-        await db.query("INSERT INTO users (username, email, password, title) VALUES ($1, $2, $3, $4)", [user, email, pass, storyline]);
+        if (account==='User'){
+            await db.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3, $4)", [user, email, pass]);
+        }
+        else if(account==='Admin')
+        {
+            await db.query("INSERT INTO admins (username, email, password) VALUES ($1, $2, $3, $4)", [user, email, pass]);
+        }
         res.render("login.ejs");
     } catch (error) {
         console.error("Error:", error);
@@ -62,23 +65,170 @@ app.post("/register", async (req, res) => {
 app.post('/login', async (req, res) => {
     const email = req.body.email;
     const pass = req.body.password;
+    const account = req.body.account;
 
     try {
-        const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
-
-        if (result.rows.length !== 0) {
-            if (result.rows[0].password === pass) {
-                req.session.user = { useremail: email };
+        let result;
+        let typeacc;
+    
+        if (account === 'User') {
+            result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+        } else if (account === 'Admin') {
+            result = await db.query("SELECT * FROM admin WHERE email = $1", [email]);
+            typeacc = 'Admin';
+        }
+    
+        if (result.rows.length > 0) {
+            const user = result.rows[0];
+    
+            if (user.password === pass) {
+                req.session.user = { useremail: email,account:account};
                 console.log(req.session.user.useremail);
-                res.render("main.ejs", {
-                    useraccount: email,
-                });
+    
+                let renderOptions = { useraccount: email };
+                if (typeacc === 'Admin') {
+                    renderOptions.account = typeacc;
+                }
+    
+                res.render("main.ejs", renderOptions);
             } else {
                 res.status(401).send("Invalid Password!!");
             }
         } else {
             res.status(404).send("Invalid email. Not registered!");
         }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+    
+});
+
+
+app.post("/title", async (req, res) => {
+    const user_email = req.session.user.useremail;
+    const account=req.session.user.account;
+    console.log(user_email)
+    let id;
+    
+    try {
+        let res2;
+        if (account==='User')
+        {
+            res2=await db.query("SELECT * FROM users WHERE email = $1", [user_email]);
+        }
+        else if(account==='Admin')
+        {
+            res2=await db.query("SELECT * FROM admins WHERE email = $1", [user_email]);
+        }
+
+        while (true) {
+            id = Math.floor(Math.random() * 25);
+            if(!ids.includes(id))
+            {
+                ids.push(id);
+                break;
+            }
+        }
+        console.log(id);
+
+        const result = await db.query("SELECT title FROM titles WHERE id=$1", [id]);
+        const line = result.rows[0].title;
+        console.log(line);
+
+        res.render("title.ejs", {
+            useraccount: user_email,
+            title1: line
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.post("/titlesave",async(req,res)=>{
+    const user_email = req.session.user.useremail;
+    const account=req.session.user.account;
+    console.log(user_email)
+    let id=ids[(ids.length)-1]
+
+    const result = await db.query("SELECT title FROM titles WHERE id=$1", [id]);
+    const line = result.rows[0].title;
+    console.log(line);
+
+    try{
+        await db.query("INSERT INTO saved (email,title) VALUES ($1,$2)" [user_email,line]);
+    }
+    catch(error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+app.post("/savedtitles", async (req, res) => {
+    const user_email = req.session.user.useremail;
+    const account = req.session.user.account;
+    console.log(user_email);
+
+    try {
+        const res3 = await db.query("SELECT * FROM saved WHERE email = $1", [user_email]);
+
+        if (res3.rows.length > 0) {
+            res.render("saved.ejs", {
+                lines: res3.rows 
+            });
+        } else {
+            res.render("saved.ejs", {
+                status:"NO SAVED STORYLINES TO DISPLAY" 
+            });
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.post("/deletesavedtitle", async (req, res) => {
+    const user_email = req.session.user.useremail;
+    const account = req.session.user.account;
+    console.log(user_email);
+    
+    const title = req.body.title;
+
+    try {
+        await db.query("DELETE FROM saved WHERE title = $1 and email=$2", [title,user_email]);
+        res.render("main.ejs", {
+            status: "Mentioned StoryLine Deleted"
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.post("/addtitle",async(req,res)=>{
+    const title=req.body.title;
+
+    try {
+        await db.query("INSERT INTO titles (title) VALUES ($1)" [title]);
+        res.render("main.ejs",{
+            status:"New StoryLine Added"
+        })
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+app.post("/deletetitle", async (req, res) => {
+    const title = req.body.title;
+
+    try {
+        await db.query("DELETE FROM titles WHERE title = $1", [title]);
+        await db.query("DELETE FROM saved WHERE title = $1", [title]);
+        res.render("main.ejs", {
+            status: "Mentioned StoryLine Deleted"
+        });
     } catch (error) {
         console.error("Error:", error);
         res.status(500).send("Internal Server Error");
@@ -94,49 +244,6 @@ app.post("/logout", (req, res) => {
             res.render("login.ejs");
         }
     });
-});
-
-app.post("/title", async (req, res) => {
-    const user_email = req.session.user.useremail;
-    console.log(user_email)
-    let id;
-
-    try {
-        const res2 = await db.query("SELECT * FROM users WHERE email = $1", [user_email]);
-
-        if (res2.rows[0].length !== 0 && res2.rows[0].title!=='story line') {
-        {
-            res.render("title.ejs", {
-                useraccount: user_email,
-                pick:'You already picked ur story Line',
-                title1: res2.rows[0].title,
-            });
-        }
-        } else {
-            while (true) {
-                id = Math.floor(Math.random() * 25); 
-                const num = await db.query("SELECT id FROM generated_numbers WHERE generated_num=$1", [id]);
-                if (num.rows.length === 0) {
-                    await db.query("INSERT INTO generated_numbers (generated_num) VALUES ($1)", [id]);
-                    break;
-                }
-            }
-            console.log(id);
-
-            const result = await db.query("SELECT title FROM titles WHERE id=$1", [id]);
-            const line = result.rows[0].title;
-            console.log(line);
-            await db.query("UPDATE users SET title = $1 WHERE email = $2", [line, user_email]);
-
-            res.render("title.ejs", {
-                useraccount: user_email,
-                title1: line
-            });
-        }
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Internal Server Error");
-    }
 });
 
 app.listen(port, () => {
